acab02feab2fd3383a6ed1315c02889d1cd45d86
Akhmad Herdian <akhmad.herdian@gmail.com>
2019-06-19 15:02:48 +0800

    Fixing test to use crud generator

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,25 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\User;
+use Laravel\BrowserKitTesting\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected $baseUrl = 'http://localhost';
+
+    protected function loginAsUser()
+    {
+        $user = $this->createUser();
+        $this->actingAs($user);
+
+        return $user;
+    }
+
+    protected function createUser()
+    {
+        return factory(User::class)->create();
+    }
 }
