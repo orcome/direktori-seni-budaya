ed8b1e9da7a5dde50c8039303508862abc0ce374
Akhmad Herdian <akhmad.herdian@gmail.com>
2019-06-24 21:30:53 +0800

    Set layout print ritual_ceremonies

--- resources/views/ritual_ceremonies/index.blade.php
+++ resources/views/ritual_ceremonies/index.blade.php
@@ -3,21 +3,24 @@
 @section('title', __('ritual_ceremony.list'))
 
 @section('content')
-<div class="mb-3">
+<div class="d-print-none mb-3">
     <div class="float-right">
         @can('create', new App\RitualCeremony)
             <a href="{{ route('ritual_ceremonies.create') }}" class="btn btn-success">{{ __('ritual_ceremony.create') }}</a>
-            <a href="{{ route('home') }}" class="btn btn-danger">{{ __('app.print') }}</a>
+            <a href="" class="btn btn-danger" onclick="window.print()">{{ __('app.print') }}</a>
             <a href="{{ route('home') }}" class="btn btn-outline-secondary">{{ __('app.back_to_menu') }}</a>
         @endcan
     </div>
     <h3 class="page-title">{{ __('ritual_ceremony.list') }} | <small>{{ __('app.total') }} : {{ $ritualCeremonies->total() }} {{ __('ritual_ceremony.ritual_ceremony') }}</small></h3>
 </div>
-
+<div class="d-none d-print-block">
+    <h5 class="text-center"><b>UPACARA RITUAL</b></h5>
+    <h5 class="text-center"><b>KABUPATEN KAPUAS</b></h5><br>
+</div>
 <div class="row">
     <div class="col-md-12">
         <div class="card">
-            <div class="card-header">
+            <div class="d-print-none card-header">
                 <form method="GET" action="" accept-charset="UTF-8" class="form-inline">
                     <div class="form-group">
                         <label for="q" class="form-label">{{ __('ritual_ceremony.search') }}</label>
@@ -34,17 +37,17 @@
                         <th>{{ __('ritual_ceremony.name') }}</th>
                         <th>{{ __('ritual_ceremony.detail') }}</th>
                         <th>{{ __('ritual_ceremony.description') }}</th>
-                        <th class="text-center">{{ __('app.action') }}</th>
+                        <th class="d-print-none text-center">{{ __('app.action') }}</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach($ritualCeremonies as $key => $ritualCeremony)
                     <tr>
                         <td class="text-center">{{ $ritualCeremonies->firstItem() + $key }}</td>
-                        <td>{!! $ritualCeremony->name_link !!}</td>
+                        <td>{{ $ritualCeremony->name }}</td>
                         <td>{{ $ritualCeremony->detail }}</td>
                         <td>{{ $ritualCeremony->description }}</td>
-                        <td class="text-center">
+                        <td class="d-print-none text-center">
                             @can('view', $ritualCeremony)
                                 <a href="{{ route('ritual_ceremonies.show', $ritualCeremony) }}" id="show-ritual_ceremony-{{ $ritualCeremony->id }}">{{ __('app.show') }}</a>
                             @endcan
