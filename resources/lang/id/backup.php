0d9d87b96ce800e2715ce1572f147f11453a2e52
Akhmad Herdian <akhmad.herdian@gmail.com>
2019-06-25 21:46:49 +0800

    Add view and lang for backup manager

---
+++ resources/lang/id/backup.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    // Labels
+    'index_title' => 'Daftar File Backup',
+    'list'        => 'Database Backup Manager',
+    'file_name'   => 'Nama File',
+    'file_size'   => 'Ukuran File',
+    'created_at'  => 'Dibuat pada',
+    'actions'     => 'Pilihan',
+    'empty'       => 'Tidak file backup yang tersedia',
+
+    // Create backup file
+    'create'      => 'Buat file backup',
+    'created'     => 'Backup file :filename telah dibuat.',
+    'not_created' => 'Backup file named :filename sudah ada.',
+
+    // Delete backup file
+    'delete'              => 'Hapus',
+    'delete_title'        => 'Hapus file backup ini',
+    'sure_to_delete_file' => 'Anda yakin ingin menghapus file <strong>":filename"</strong>?',
+    'cancel_delete'       => 'Batal Menghapus',
+    'confirm_delete'      => 'YA, Silakan delete file ini!',
+    'delete_confirm'      => 'Tekan OK to Menghapus.',
+    'deleted'             => 'File Backup :filename berhasil didelete!',
+
+    // Download backup file
+    'download' => 'Download',
+
+    // Restore backup
+    'restore'         => 'Restore',
+    'restore_title'   => 'Restore database dari file',
+    'sure_to_restore' => 'Apkah Anda yakin ingin restore database menggunakan file backup ini "<strong>:filename</strong>"? <br><br>Please make sure your <strong>current database has been backed up</strong>.',
+    'cancel_restore'  => 'Batal Restore',
+    'confirm_restore' => 'Ya, Restore Database!',
+    'restore_confirm' => 'Tekan OK untuk Restore.',
+    'restored'        => 'Database telah direstore menggunakan file backup :filename',
+
+    // Upload backup fle
+    'upload'   => 'Upload Backup File',
+    'uploaded' => 'Backup file :filename telah di upload.',
+];
---
+++ resources/views/backups/index.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+@section('title', __('backup.index_title'))
+
+@section('content')
+
+@include('layouts.partials.setting_nav')
+
+<div class="row">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">{{ __('backup.index_title') }}</div>
+            <table class="table table-sm table-responsive-sm">
+                <thead>
+                    <th class="text-center">{{ __('app.table_no') }}</th>
+                    <th>{{ __('backup.file_name') }}</th>
+                    <th>{{ __('backup.file_size') }}</th>
+                    <th>{{ __('backup.created_at') }}</th>
+                    <th class="text-center">{{ __('backup.actions') }}</th>
+                </thead>
+                <tbody>
+                    @forelse($backups as $key => $backup)
+                    <tr>
+                        <td class="text-center">{{ $key + 1 }}</td>
+                        <td>{{ $backup->getFilename() }}</td>
+                        <td>{{ format_size_units($backup->getSize()) }}</td>
+                        <td>{{ date('Y-m-d H:i:s', $backup->getMTime()) }}</td>
+                        <td class="text-center">
+                            <div class="btn-group" role="group">
+                                <a href="{{ route('backups.index', ['action' => 'restore', 'file_name' => $backup->getFilename()]) }}"
+                                    id="restore_{{ str_replace('.gz', '', $backup->getFilename()) }}"
+                                    class="btn btn-warning btn-sm"
+                                    title="{{ __('backup.restore') }}">{{ __('backup.restore') }}</a>
+                                <a href="{{ route('backups.download', [$backup->getFilename()]) }}"
+                                    id="download_{{ str_replace('.gz', '', $backup->getFilename()) }}"
+                                    class="btn btn-success btn-sm"
+                                    title="{{ __('backup.download') }}">{{ __('backup.download') }}</a>
+                                <a href="{{ route('backups.index', ['action' => 'delete', 'file_name' => $backup->getFilename()]) }}"
+                                    id="del_{{ str_replace('.gz', '', $backup->getFilename()) }}"
+                                    class="btn btn-danger btn-sm"
+                                    title="{{ __('backup.delete') }}">{{ __('backup.delete') }}</a>
+                            </div>
+                        </td>
+                    </tr>
+                    @empty
+                    <tr>
+                        <td colspan="5">{{ __('backup.empty') }}</td>
+                    </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="col-md-4">
+        @include('backups.forms')
+    </div>
+</div>
+@endsection
